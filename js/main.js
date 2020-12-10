ef4eefb990e5eb2b75805233e4118631a5918068
karimishiva <60754344+karimishiva@users.noreply.github.com>
2020-12-11 00:32:35 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
 
-function myFunc(txt) {
-    var buy = document.getElementById("btn-1");
-    var sell = document.getElementById("btn-2");
-    var rent = document.getElementById("btn-3");
-    var buy2 = document.getElementById("buy-btn");
-    var sell2 = document.getElementById("sell-btn");
-    var rent2 = document.getElementById("rent-btn");
+const myFunc = (txt) => {
+    let buy = document.getElementById("btn-1");
+    let sell = document.getElementById("btn-2");
+    let rent = document.getElementById("btn-3");
+    let buy2 = document.getElementById("buy-btn");
+    let sell2 = document.getElementById("sell-btn");
+    let rent2 = document.getElementById("rent-btn");
+
+
 
     if (txt == 'sell') {
         document.getElementById("main-text").innerHTML = "Buy and sell your textbooks";
